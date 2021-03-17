bf271eb129588c0e93e1af8f569d322aa226ca86
xhoto <xhoto.choi@gmail.com>
2021-03-17 17:09:23 +0900

    remove duplicated module

--- azure/terraform/modules/mgmt/container_registry/main.tf
+++ azure/terraform/modules/mgmt/container_registry/main.tf
@@ -2,14 +2,14 @@ module "global_variables" {
   source="../global_variables"
 }
 
-locals {
-  azurerm_container_registry_sku = "Premium" # Basic, Standard, Premium
-}
-
 resource "azurerm_container_registry" "acr" {
   name                     = var.name
   resource_group_name      = var.rg.name
   location                 = var.rg.location
-  sku                      = var.sku
-  tags                     = module.global_variables.tags
+  sku                      = var.sku 
+  admin_enabled            = false
+}
+
+output "instance" {
+  value = azurerm_container_registry.acr
 }
--- azure/terraform/modules/mgmt/container_registry/variables.tf
+++ azure/terraform/modules/mgmt/container_registry/variables.tf
@@ -3,10 +3,10 @@ variable "name" {
 }
 
 variable "rg" {
-  description = "The resource group for container registry"
+  description = "The resoure group for container registry"
 }
 
 variable "sku" {
-  description = "container registry sku" # Basic, Standard, Premium
-  default = "Premium"
+  description = "The sku for container registry"
+  default = "Basic"
 }
